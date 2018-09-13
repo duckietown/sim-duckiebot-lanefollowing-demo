225dccd944482e23a03d673a8fa9524700bd4ec7
Bhairav Mehta <bhairavmehta95@gmail.com>
2018-09-12 23:26:38 -0400

    adding updated flow

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
-#FROM duckietown/rpi-ros-kinetic-base
 FROM ros:kinetic-perception
-#RUN [ "cross-build-start" ]
 
 WORKDIR /workspace
-RUN git clone https://github.com/duckietown/gym-duckietown-agent agent
 
 RUN apt-get update -y && \
     apt-get install -y --no-install-recommends git \
@@ -20,15 +17,7 @@ RUN apt-get update -y && \
     apt-get remove -y git && \
     rm -rf /var/lib/apt/lists/*
 
-COPY agent-ros.py agent
-COPY ros-envs/envs agent/gym_duckietown_agent/envs
-COPY ros-envs/__init__.py agent/gym_duckietown_agent/__init__.py
+COPY . agent
 RUN pip install -e agent
-
-#RUN [ "cross-build-end" ]
-
-#COPY ./ros_entrypoint.sh /
-#ENTRYPOINT ["/ros_entrypoint.sh"]
-
-CMD ["/bin/bash"]
+CMD python agent/rosbridge.py
 
--- rosbridge.py
+++ rosbridge.py
@@ -1,5 +1,5 @@
 from duckietown_slimremote.networking import make_sub_socket, make_pub_socket, construct_action, recv_array, \
-    get_ip
+    get_ip, recv_gym
 from duckietown_slimremote.helpers import random_id, timer
 
 import time
@@ -11,14 +11,15 @@ from duckietown_slimremote.helpers import random_id, get_py_version
 import rospy
 from std_msgs.msg import Bool, String
 from sensor_msgs.msg import Image
+import numpy as np
 
 
 class ROSBridge(object):
     def __init__(self):
         self.context = zmq.Context()
 
-        self.image_sub = make_sub_socket(for_images=True)
-        self.action_pub = make_pub_socket(context=self.context)
+        self.image_sub = make_sub_socket(target="gym-duckietown-server", for_images=True)
+        self.action_pub = make_pub_socket(context_=self.context)
 
         self.poller = zmq.Poller()
         self.poller.register(self.image_sub, zmq.POLLIN)
@@ -32,23 +33,22 @@ class ROSBridge(object):
         print("sending init", msg)
 
         # init
-        action_pub.send_string(msg)
+        self.action_pub.send_string(msg)
 
         self.action_sub = rospy.Subscriber('/1', String, self._action_cb)
-        self.reset_sub = rospy.Subscriber('/2', Bool, self._reset_cb)
+        # self.reset_sub = rospy.Subscriber('/2', Bool, self._reset_cb)
 
         self.cam_pub = rospy.Publisher('/img', Image, queue_size=10)
-        self.action_debug_pub = rospy.Publisher('/1', String, queue_size=10)
-        self.action = construct_action(own_id, own_ip, [0, 0])
-        self.r = rospy.Rate(60)
+        self.action = construct_action(self.own_id, self.own_ip, [0, 0])
 
         rospy.init_node('RemoteRobotRos')
+        self.r = rospy.Rate(60)
 
     def _action_cb(self, msg):
         action = msg.data.split()
         assert len(action) == 2
 
-        self.action = construct_action(own_id, own_ip, action)
+        self.action = construct_action(self.own_id, self.own_ip, action)
         
 
     def _publish_img(self, obs):
@@ -62,20 +62,18 @@ class ROSBridge(object):
             obs.dtype.byteorder == '>' or
             obs.dtype.byteorder == '=' and sys.byteorder == 'big'
         )
-
+        
         self.cam_pub.publish(img_msg)
 
-    def spin():
+    def spin(self):
         while not rospy.is_shutdown():
-            action_pub.send_string(self.action)
+            self.action_pub.send_string(self.action)
             socks = dict(self.poller.poll(5))
-            if image_sub in socks and socks[image_sub] == zmq.POLLIN:
-                img = recv_array(image_sub)
+            if self.image_sub in socks and socks[self.image_sub] == zmq.POLLIN:
+                img, r , d , _ = recv_gym(self.image_sub)
                 self._publish_img(img)
 
             self.r.sleep()
 
-
-if __name__ == '__main__':
-    r = ROSBridge()
-    r.spin()
+r = ROSBridge()
+r.spin()
