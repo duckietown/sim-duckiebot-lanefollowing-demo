a77a728007d0aaebf00e0b309c61db951e4c101e
Bhairav Mehta <bhairavmehta95@gmail.com>
2018-10-10 21:23:59 -0400

    right msg types, need to hook into stack

--- action_test.py
+++ action_test.py
@@ -2,6 +2,7 @@ import rospy
 from std_msgs.msg import Bool, String
 from sensor_msgs.msg import Image
 import numpy as np
+from duckietown_msgs.msg import Twist2DStamped, WheelsCmdStamped
 
 import gym
 import gym_duckietown_agent  # don't remove this line
@@ -17,12 +18,18 @@ def main():
     # this first to make sure the sim is reset.
     # env.reset()
 
-    action_debug_pub = rospy.Publisher('/1', String, queue_size=10)
+    action_debug_pub = rospy.Publisher('/1', Twist2DStamped, queue_size=10)
     rospy.init_node('ActionServer')
-    while True:
-        action = "{} {}".format(np.random.random(), np.random.random())
+    action = Twist2DStamped()
+    r = rospy.Rate(60)
+    while not rospy.is_shutdown():
+        action.v = np.random.random()
+        action.omega = np.random.random()
+        action.header.stamp = rospy.Time.now()
+        
         action_debug_pub.publish(action)
-        # env.step([np.random.random(), np.random.random()])
+    
+        r.sleep()
 
 if __name__ == '__main__':
     main()
--- rosbridge.py
+++ rosbridge.py
@@ -1,9 +1,11 @@
 from duckietown_slimremote.pc.robot import RemoteRobot
 import rospy
 from std_msgs.msg import Bool, String, Header
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from duckietown_msgs.msg import Twist2DStamped, WheelsCmdStamped
 import numpy as np
 import os
+import cv2
 
 
 class ROSBridge(object):
@@ -12,37 +14,30 @@ class ROSBridge(object):
         host = os.getenv("DUCKIETOWN_SERVER", "localhost")
         # Create ZMQ connection
         self.sim = RemoteRobot(host, silent=False)
-        self.action_sub = rospy.Subscriber('/1', String, self._action_cb)
+        self.action_sub = rospy.Subscriber('/1', Twist2DStamped, self._action_cb)
 
-        self.cam_pub = rospy.Publisher('/img', Image, queue_size=10)
-        self.action_debug_pub = rospy.Publisher('/1', String, queue_size=10)
+        self.cam_pub = rospy.Publisher('/corrected_image/compressed', CompressedImage, queue_size=10)
         self.action = np.array([0, 0])
 
         rospy.init_node('RemoteRobotRos')
         self.r = rospy.Rate(60)
 
     def _action_cb(self, msg):
-        action = msg.data.split()
-        assert len(action) == 2
-        # print("Got action {}".format(action))
-        self.action = np.array(action)
+        v = msg.v
+        omega = msg.omega
+        self.action = np.array([v, omega])
         
 
     def _publish_img(self, obs):
-        # Hardcoded Implementation of ros_numpy's ImageConverter
-        img_msg = Image(encoding='rgb8')
-        img_msg.height, img_msg.width, _ = obs.shape
-        contig = np.ascontiguousarray(obs)
-        img_msg.data = contig.tostring()
-        img_msg.step = contig.strides[0]
-        img_msg.is_bigendian = (
-            obs.dtype.byteorder == '>' or
-            obs.dtype.byteorder == '=' and sys.byteorder == 'big'
-        )
+        img_msg = CompressedImage()
 
         time = rospy.get_rostime()
         img_msg.header.stamp.secs = time.secs
         img_msg.header.stamp.nsecs = time.nsecs
+
+        img_msg.format = "jpeg"
+        contig = np.ascontiguousarray(np.roll(obs, 2, axis=-1))
+        img_msg.data = np.array(cv2.imencode('.jpg', contig)[1]).tostring()
   
         self.cam_pub.publish(img_msg)
 
